27ad6bdaad7063908b14c16389c25e4a55ae2e2c
mattimatti <mmonti@gmail.com>
2014-06-23 19:36:57 +0200

    editform

---
+++ templates/structured/views/editform.js
@@ -0,0 +1,63 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  var app = require("app");
+
+
+	var Layout = Backbone.View.extend({
+
+		<%  if(options.backbone.layoutmanager){ %>
+
+		/************ Backbone.LayoutManager *****************/
+
+		manage: true,
+
+		el: false,
+
+		template: require(["ldsh!./template"]),
+
+
+		beforeRender: function() {
+
+
+		},
+
+		afterRender: function() {
+
+
+		},
+
+
+		serialize: function() {
+		   return { model: this.model };
+		},
+
+
+		<% }else{ %>
+
+		manage: false,
+
+		<% } %>
+
+
+		/************ Backbone.View *****************/
+
+
+		events: {
+      		click: "sayHello"
+    	},
+
+    	sayHello: function() {
+
+
+		},
+
+		initialize: function() {
+
+
+		}
+
+	});
+
+	module.exports  = Layout;
+});
